9e972ba1acb0cd57eca387924834f31545a36468
Mrinali_git <mrinalikmr@gmail.com>
2026-08-30 15:01:49 +0530

    Update project1.c
    fixed syntax error, added switch statement , implemented case 1 and case 2

--- project1.c
+++ project1.c
@@ -9,7 +9,7 @@ int main()
     int saved_pin = 2367; // Initiallization
     int enterpin, choice;
     int balance = 5000;
-
+   
     printf(" enter the saved_pin");
     scanf("%d", &enterpin);
 
@@ -19,18 +19,26 @@ int main()
        // Display option   ( using do while syntax; post text syntax)
     do
     {
-        printf("---ATM MENU--");
+        printf("\n---ATM MENU--\n");
         printf("1.check balance\n");
-        printf("2.Deposit Money");
+        printf("2.Deposit Money\n");
         printf("3.withdraw money\n");
         printf("4.EXIT\n");
         printf("5. choose an  option ");
         scanf("%d", &choice);
 
-        switch (choice){
-            case1:
-            printf("current balance:%d", balance);
-            
+        switch (choice)  {
+            case 1:          // there must gap between case and no 
+            printf("\ncurrent balance:%d", balance);
+            break;
+            case 2: {
+            int deposit;    // to declare int inside case 2 use braces
+            printf(" ENTER THE AMOUNT TO DEPOSIT");
+            scanf("%d", &deposit);
+            balance+=deposit;
+            printf(" sucessfully deposit,  balance :%d",balance);
+            break;
+            }
             
         }
     } while (choice != 4);
